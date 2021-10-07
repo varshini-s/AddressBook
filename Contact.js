1db4541dbcbab9c61bccca2ce60964728f4c10bb
varshini <dreamvarshini@gmail.com>
2021-10-07 20:19:33 +0530

    ADDED: defining constructor for contacts

---
+++ Contact.js
@@ -0,0 +1,19 @@
+class Contact 
+{
+
+    constructor(...params) 
+    {
+        this.firstName = params[0]
+        this.lastName = params[1]
+        this.address = params[2]
+        this.city = params[3]
+        this.state = params[4]
+        this.zip = params[5]
+        this.phoneNumber = params[6]
+        this.email = params[7]
+    }
+
+    
+
+
+}
