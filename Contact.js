e3bb0f71e2110c3b11239845f11b384016aaec07
varshini <dreamvarshini@gmail.com>
2021-10-07 20:20:02 +0530

    ADDED: getters and setters for contacts

--- Contact.js
+++ Contact.js
@@ -13,7 +13,70 @@ class Contact
         this.email = params[7]
     }
 
-    
+    get firstName()
+    {
+        return this._firstName;
+    }
+    set firstName(firstName)
+    {
+        this._firstName=firstName;
+    }
+    get lastName()
+    {
+        return this._lastName;
+    }
+    set lastName(lastName)
+    {
+        this._lastName=lastName;
+    }
+    get address()
+    {
+        return this._address;
+    }
+    set address(address)
+    {
+        this._address=address;
+    }
+    get city()
+    {
+        return this._city;
+    }
+    set city(city)
+    {
+        this._city=city;
+    }
+    get state()
+    {
+        return this._state;
+    }
+    set state(state)
+    {
+        this._state=state;
+    }
+    get zip()
+    {
+        return this._zip;
+    }
+    set zip(zip)
+    {
+        this._zip=zip;
+    }
+    get phoneNumber()
+    {
+        return this._phoneNumber;
+    }
+    set phoneNumber(phoneNumber)
+    {
+        this._phoneNumber=phoneNumber;
+    }
+    get email()
+    {
+        return this._email;
+    }
+    set email(email)
+    {
+        this._email=email;
+    }
 
 
 }
